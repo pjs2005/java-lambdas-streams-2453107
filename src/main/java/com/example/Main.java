c15edbdad296ebeab64093bd88295b2ae2c8e193
pjs2005 <pjs2005@users.noreply.github.com>
2026-06-08 18:36:32 +0100

    changes

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,5 +1,9 @@
 package com.example;
 
+<<<<<<< Updated upstream
+=======
+import java.util.Random;
+>>>>>>> Stashed changes
 import java.util.function.IntBinaryOperator;
 
 public class Main {
@@ -11,7 +15,25 @@ public class Main {
     Greeting greeting2 = () -> System.out.println("Hello From Poojan");
     greeting2.sayHello();
 
+<<<<<<< Updated upstream
     Calcuator calc = (int a, int b) -> a + b;
+=======
+    Greeting greeting3 = () -> System.out.println("Hello From here!");
+    greeting3.sayHello();
+
+    IntBinaryOperator calculator = (int x, int y) -> {
+      int result = 0;
+      Random random = new Random();
+      result += random.nextInt(50) + (x * y);
+      return result;
+
+    };
+    System.out.println(calculator.applyAsInt(1,2));
+
+
+
+  }
+>>>>>>> Stashed changes
 
     IntBinaryOperator calc2 = (int a, int b) -> a + b;
 
