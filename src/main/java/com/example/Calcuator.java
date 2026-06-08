d5d003cb2ae35bca24171a7afc700a472db07b13
pjs2005 <pjs2005@users.noreply.github.com>
2026-06-08 18:36:25 +0100

    changes

---
+++ src/main/java/com/example/Calcuator.java
@@ -0,0 +1,7 @@
+package com.example;
+
+@FunctionalInterface
+public interface Calcuator {
+
+    int calucate(int a, int b);
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,20 +1,22 @@
 package com.example;
 
+import java.util.function.IntBinaryOperator;
+
 public class Main {
 
   public static void main(String[] args) {
-
     Greeting greeting = new HelloWorldGreeting();
     greeting.sayHello();
 
-    Greeting greeting2 = new Greeting() {
-      @Override
-      public void sayHello() {
-        System.out.println("Hello World");
-      }
-    };
+    Greeting greeting2 = () -> System.out.println("Hello From Poojan");
     greeting2.sayHello();
 
-  }
+    Calcuator calc = (int a, int b) -> a + b;
 
+    IntBinaryOperator calc2 = (int a, int b) -> a + b;
+
+    System.out.println(calc.calucate(1,2));
+    System.out.println(calc2.applyAsInt(1,2));
+
+  }
 }
